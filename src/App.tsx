bffa9be41236b6d69d2fd468a0389d2be5cb26dd
JoshMusira <joshuamusira01@gmail.com>
2023-07-31 16:14:22 +0300

    feat: Configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
+import { routes } from './routes/Routes'
 
 function App() {
 
-  return (
-    <>
-    
-    </>
-  )
+  return <RouterProvider router={routes} />
+
 }
 
 export default App
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import Dashlayout from "../layouts/Dashlayout";
+import MsgsLayout from "../layouts/MsgsLayout";
+import Chat from "../components/chat/Chat";
+import Settings from "../components/Settings";
+import Login from "../pages/auth/Login";
+import Signup from "../pages/auth/Signup";
+export const routes = createBrowserRouter(
+    createRoutesFromElements(
+        <Route >
+            <Route path="/auth">
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<Signup />} />
+            </Route>
+
+            <Route path="/dashboard" element={<Dashlayout />}>
+                <Route path="messages" element={<MsgsLayout />}>
+                    <Route path=":msgID" element={<Chat />} />
+                </Route>
+                <Route path="settings" element={<Settings />} />
+            </Route>
+
+        </Route>
+    )
+)
