181d5c731a52dd8a8850137ac02d7718ce8f9b93
JoshMusira <joshuamusira01@gmail.com>
2023-08-02 15:38:30 +0300

    feat: implement message

--- src/components/messages/Msg.tsx
+++ src/components/messages/Msg.tsx
@@ -1,7 +1,21 @@
+import profile from '../../assets/software.jpg'
 const Msg = () => {
   return (
-    <div>Msg</div>
+    <div className="flex m-[5px] w-[100%]  ">
+      <div className=" ml-[5px] w-[20%] "><img
+        className='w-20 h-20 rounded-full object-cover' src={profile} alt="" /></div>
+      <div className="flex w-[80%] justify-between m-[6px]">
+        <div className="flex flex-col gap-4">
+          <p className='font-bold '>Josh Musira</p>
+          <p className='text-green-500'>Typing...</p>
+        </div>
+        <div className="flex text-center mr-[5px]  flex-col gap-4">
+          <p className='text-gray-400'>02:21 PM</p>
+          <p className='w-6 h-6 text-white rounded-full bg-red-600'>2</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Msg
+export default Msg;
--- src/components/messages/MsgList.tsx
+++ src/components/messages/MsgList.tsx
@@ -3,7 +3,7 @@ import MsgListHeader from "./MsgListHeader"
 
 const MsgList = () => {
   return (
-    <div className='w-[40%]'>
+    <div className='w-[40%] h-[100vh] bg-gray-50 shadow-lg'>
       <MsgListHeader />
       <Msg />
       <Msg />
