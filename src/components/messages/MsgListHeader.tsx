d399d5b257d36bc83ec90511626e4d485e4b585f
JoshMusira <joshuamusira01@gmail.com>
2023-08-02 14:02:08 +0300

    feat: fix MsgsLayout header

--- src/components/messages/MsgListHeader.tsx
+++ src/components/messages/MsgListHeader.tsx
@@ -2,10 +2,10 @@ import { AiOutlineEdit, AiOutlineSearch } from 'react-icons/ai'
 const MsgListHeader = () => {
   return (
     <div className='flex justify-between gap-5 mt-5 w-[100%]'>
-      <p className='text-[#0147ff] text-2xl'>Messages</p>
-      <div className='flex items-center gap-4'>
-        <AiOutlineEdit />
-        <AiOutlineSearch />
+      <p className='text-[#0147ff] text-2xl font-bold'>Messages</p>
+      <div className='flex items-center gap-4 text-gray-300 text-2xl '>
+        <span className='bg-gray-100 rounded-full p-[4px]'><AiOutlineEdit /></span>
+        <span className='bg-gray-100 rounded-full p-[4px]'><AiOutlineSearch /></span>
       </div>
     </div>
   )
